26bf475448739599f9f2a847f23e3625af51b7d3
NguyenNhiB1909962 <nguyennhi325380@gmail.com>
2022-09-23 09:13:53 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const cors = require("cors");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
